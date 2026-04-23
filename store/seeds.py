4c2ad36ec678a38e94a29e83a58a944700bfdc3e
Roshan Saganti <roshansaganti@hotmail.com>
2026-04-22 20:37:42 -0400

    Generate custom fake data for Customer model

--- store/seeds.py
+++ store/seeds.py
@@ -15,7 +15,17 @@ seeder = Seed.seeder()
 
 
 # Seed data for each model
-seeder.add_entity(Customer, 5)
+seeder.add_entity(
+    Customer,
+    5,
+    {
+        "name": lambda x: seeder.faker.name(),
+        "email": lambda x: seeder.faker.email(),
+        "password": lambda x: seeder.faker.password(),
+        "phone_number": lambda x: seeder.faker.phone_number(),
+        "date_registered": lambda x: seeder.faker.date_time_this_year(),
+    },
+)
 seeder.add_entity(Address, 5)
 seeder.add_entity(Category, 5)
 seeder.add_entity(Product, 5)
