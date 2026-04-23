1cc56c46797cb342eafb5a60ca352c25ba8bb333
Roshan Saganti <roshansaganti@hotmail.com>
2026-04-22 20:06:15 -0400

    Generate fake data into database

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 uv run python manage.py migrate
 
 # Seed the database
-# uv run python manage.py seed
+uv run python manage.py seed store --number=5
 
 # Start Django server
 uv run python manage.py runserver 0.0.0:8000
